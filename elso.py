c561e271d3916c11d46b8c7ee7c224663ef753d1
adamcsik <adamcsik@uniduna.hu>
2023-09-14 16:22:15 +0200

    2 szinkron

--- elso.py
+++ elso.py
@@ -1,1 +1,20 @@
-print('Üdv a fedélzeten')
+# print('Üdv \na \nfedélzeten', end='')
+"""print('\nSzia', 'Jenő',sep='-----')
+
+print('''Ez
+több
+sorba
+kerül
+''')
+"""
+
+# nev = input('Hogy hívnak: ')
+nev = 'valaki'
+titulus = 'dr.'
+print('Szia', nev)
+print(f'Szia {nev}')
+print('Helló {0} {1}'.format(titulus, nev))
+
+print('jobbra'.rjust(50, '-'))
+print('balra'.ljust(50,'-'))
+print('közép'.center(50))

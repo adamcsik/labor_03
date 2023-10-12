0ba15f67c1ae4399356f08b161611450a8d06c2c
adamcsik <adamcsik@uniduna.hu>
2023-10-12 16:16:26 +0200

    6 szinkron

--- gyak_05.py
+++ gyak_05.py
@@ -1,15 +1,20 @@
 # Beléptetőrendszer
 def regisztracio():
-    felhasznalonev()
-    jelszo_bekerese()
-    jelszo_ellenorzese()
-    ok_regisztracio = True
+    felhasznalo_email = felhasznalonev()
+    felhasznalo_jelszo = jelszo_bekerese()
+    if jelszo_ellenorzese(felhasznalo_jelszo, 3):
+        ok_regisztracio = True
+        with open("felhasznalok.csv", "a", encoding="utf-8") as fajl:
+            fajl.write(felhasznalo_email + ";" + felhasznalo_jelszo + "\n")
+    else:
+        ok_regisztracio = False
     return ok_regisztracio
 
 def felhasznalonev():
     felhasznalo_email = input("Kérem az email címét: ")
     while " " in felhasznalo_email or "@" not in felhasznalo_email or "." not in felhasznalo_email:
         felhasznalo_email = input("Nem megfelelő az amail!!!\nKérem az email címét: ")
+    return felhasznalo_email
 
 def jelszo_bekerese():
     ok_jelszo = True
@@ -17,27 +22,54 @@ def jelszo_bekerese():
     while ok_jelszo:
         if len(felhasznalo_jelszo) < 8:
             ok_jelszo = False
+
         van = 0
         for i in range(len(felhasznalo_jelszo)):
             if felhasznalo_jelszo[i].isnumeric():
                 van += 1
         if van == 0:
             ok_jelszo = False
+
+        van = 0
+        for i in range(len(felhasznalo_jelszo)):
+            if felhasznalo_jelszo[i].isupper():
+                van += 1
+        if van == 0:
+            ok_jelszo = False
+
+        van = 0
+        for i in range(len(felhasznalo_jelszo)):
+            if felhasznalo_jelszo[i].islower():
+                van += 1
+        if van == 0:
+            ok_jelszo = False
+
         if ok_jelszo:
             ok_jelszo = False
         else:
             felhasznalo_jelszo = input("Nem jó a jelszó!!!\nKérek egy jelszót (1,a,A, min 8 kar.): ")
             ok_jelszo = True
-def jelszo_ellenorzese():
-    pass
+    return felhasznalo_jelszo
+
+def jelszo_ellenorzese(felhasznalo_jelszo, probalkozas):
+    jelszo2 = input("Kérem ismét írja be a jelszót: ")
+    i = 1
+    while jelszo2 != felhasznalo_jelszo and i < probalkozas:
+        jelszo2 = input("Nem megfelelő a jelszó!\nKérem ismét írja be a jelszót: ")
+        i += 1
+    if jelszo2 == felhasznalo_jelszo:
+        egyezes = True
+    else:
+        egyezes = False
+    return egyezes
 
 def beleptetes():
     pass
 
 
 # Innen indul majd a program
-felhasznalo_email = ""
-felhasznalo_jelszo = ""
-jelszo_bekerese()
 if regisztracio():
+    print("Sikeres volt a regisztráció!\nElindítjuk az első beléptetést!")
     beleptetes()
+else:
+    print("Nem sikerült a regisztráció! Próbálja meg újra!")
